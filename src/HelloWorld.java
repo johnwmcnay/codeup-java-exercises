bb8cf5eaee98d580d9f6763202a71d6d7f4e274b
John McNay <john.w.mcnay@gmail.com>
2020-09-28 15:22:42 -0500

    finished Java data types exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,38 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, ");
-        System.out.print("World!");
+//        int myFavoriteNumber = 13;
+//        String myString = "John";
+//        double myNumber = 3.14;
+//
+//        System.out.print("Hello, ");
+//        System.out.print(myString);
+//        System.out.print(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+
+//        int z = Integer.MAX_VALUE + 1;
+//        System.out.print(z);
     }
 }
