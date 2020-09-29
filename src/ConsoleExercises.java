deb635e3618376248af43b962c31d37ca3ff612a
John McNay <john.w.mcnay@gmail.com>
2020-09-29 14:18:42 -0500

    completed bonus exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -25,5 +25,18 @@ public class ConsoleExercises {
         String sentence = scanner.nextLine();
 
         System.out.println(sentence);
+
+        System.out.println("Enter the dimensions (width and height)");
+        String num1 = scanner.next();
+        String num2 = scanner.next();
+
+        int width = Integer.parseInt(num1);
+        int length = Integer.parseInt(num2);
+
+        int area = width * length;
+        int perimeter = 2 * (width + length);
+
+        System.out.printf("A room that is %s by %s has an area of %s square units and a perimeter of %s.", width, length, area, perimeter);
+
     }
 }
