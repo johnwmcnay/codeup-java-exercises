375695da28bc4b398d240192bef98801146ea081
John McNay <john.w.mcnay@gmail.com>
2020-10-01 13:33:14 -0500

    completed excercise 1b

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,30 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
 
-        while (i <= 15) {
+        int i = 0;
+
+        do {
             System.out.println(i);
-            i++;
-        }
+            i += 2;
+        } while (i < 100);
+
+        do {
+            System.out.println(i);
+            i -= 5;
+        } while (i >= -10);
+
+        long y = 2;
+        do {
+            System.out.println(y);
+            y = y * y;
+        } while (y < 1000000);
+
+//exercise 1a
+//        int i = 5;
+//
+//        while (i <= 15) {
+//            System.out.printf("%s ", i);
+//            i++;
+//        }
     }
 }
