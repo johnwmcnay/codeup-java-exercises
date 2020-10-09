a51b60543ccd63dc46a69a8a54de566678ba62e2
John McNay <john.w.mcnay@gmail.com>
2020-10-09 12:29:09 -0500

    practice code plus abstract and interfaces exercise completed

---
+++ src/Manager.java
@@ -0,0 +1,9 @@
+class Manager extends Employee {
+    public Manager(String name, String department) {
+        super(name, department);
+    }
+
+    public String work() {
+        return "holding meetings";
+    }
+}
