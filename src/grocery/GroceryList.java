a0467cd1c9c31b4653dfbb756a1ef141d8dd6da6
John McNay <john.w.mcnay@gmail.com>
2020-10-13 15:18:38 -0500

    collections exercise completed

--- src/grocery/GroceryList.java
+++ src/grocery/GroceryList.java
@@ -2,18 +2,17 @@ package grocery;
 
 import util.Input;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class GroceryList {
 
-    public static final ArrayList<String> CATEGORIES = new ArrayList<String>(
-            Arrays.asList("meat", "dairy", "produce", "frozen", "canned", "bakery"));
+    public static List<String> CATEGORIES = Arrays.asList("meat", "dairy", "produce", "frozen", "canned", "bakery");
     private final HashMap<String, GroceryItem> groceryList = new HashMap<>();
 
-    public GroceryList(GroceryItem ...items) {
-        for (GroceryItem item: items) {
+    public GroceryList(GroceryItem... items) {
+        Collections.sort(CATEGORIES);
+        for (GroceryItem item : items) {
             addItem(item);
         }
     }
@@ -47,16 +46,34 @@ public class GroceryList {
 
     public static void printCategories() {
         System.out.print("\n");
-        for (String category: CATEGORIES) {
+        for (String category : CATEGORIES) {
             System.out.println(category);
         }
     }
 
     public void print() {
-        for (GroceryItem item: this.getList().values()) {
-            System.out.printf("Name: %s - Quantity: %d - Category: %s\n",
-                    item.getName(), item.getQuantity(), item.getCategory());
-        }
-    }
+        for (String category : CATEGORIES) {
 
+            List<String> list = new ArrayList<>();
+
+            for (GroceryItem item : this.getList().values()) {
+                if (item.getCategory().equalsIgnoreCase(category)) {
+                    list.add(item.getName());
+                }
+            }
+            Collections.sort(list);
+
+            if (list.size() > 0) {
+                System.out.printf("=====%s=====\n", category);
+                for (String str : list) {
+                    GroceryItem item = groceryList.get(str);
+
+                    System.out.printf("Name: %s - Quantity: %d\n",
+                            item.getName(), item.getQuantity());
+                }
+                System.out.print("\n");
+            }
+        }
+
+    }
 }
